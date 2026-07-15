b84fc055e55dc9eb538afa505dde440ab6036c30
Zaynrr <berhandharyokrishnanda@gmail.com>
2026-07-15 22:21:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import os
 from dotenv import load_dotenv
 
-# Load file .env terlebih dahulu
+# Load file .env 
 load_dotenv()
 
 def create_app():
-    # Memanggil factory function dari dalam folder backend
     from backend import create_app as backend_create_app
     return backend_create_app()
 
-# Vercel mencari variabel bernama "app" di tingkat paling atas (top-level)
 app = create_app()
 
 if __name__ == '__main__':
